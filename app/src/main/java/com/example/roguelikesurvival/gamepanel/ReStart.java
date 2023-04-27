6c980571aafa2a0e9f89908ef6529eca8e7af127
Taeyu Kim <kty0627@ajou.ac.kr>
2023-04-28 01:02:03 +0900

    background

---
+++ app/src/main/java/com/example/roguelikesurvival/gamepanel/ReStart.java
@@ -0,0 +1,28 @@
+package com.example.roguelikesurvival.gamepanel;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.roguelikesurvival.MainActivity;
+import com.example.roguelikesurvival.R;
+
+public class ReStart extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView((R.layout.restart));
+
+        overridePendingTransition(R.anim.fade_in, R.anim.no_anim);
+    }
+
+    public void restartGame(View view){
+        Intent intent = new Intent(this, StartUp.class);
+
+        startActivity(intent);
+
+        finish();
+    }
+}
