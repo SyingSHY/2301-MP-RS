653439f0fcb3f68e75479fed4c13caef83ee4db5
hyeonseob22 <hyeonseob22@gmail.com>
2023-06-08 00:30:08 +0900

    update skill button img

--- app/src/main/java/com/example/roguelikesurvival/gamepanel/SkillButton.java
+++ app/src/main/java/com/example/roguelikesurvival/gamepanel/SkillButton.java
@@ -1,10 +1,14 @@
 package com.example.roguelikesurvival.gamepanel;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.SystemClock;
 
 import com.example.roguelikesurvival.Game;
+import com.example.roguelikesurvival.R;
 import com.example.roguelikesurvival.Utils;
 
 public class SkillButton {
@@ -15,6 +19,11 @@ public class SkillButton {
     private boolean isPressed;
 
     private Game game;
+    private Bitmap buttonBitmap;
+    private Bitmap buttonPressedBitmap;
+
+    private long lastClickTime;
+    private long skillCooldown = 30000;  // 쿨타임: 30초
 
     public SkillButton(int centerX, int centerY, int radius, Game game) {
         this.centerX = centerX;
@@ -22,14 +31,22 @@ public class SkillButton {
         this.radius = radius;
         this.game = game;
 
-
         paint = new Paint();
         paint.setColor(Color.WHITE);
         paint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        // 버튼 이미지 로드
+        buttonBitmap = BitmapFactory.decodeResource(game.getContext().getResources(), R.drawable.green_button);
+        buttonPressedBitmap = BitmapFactory.decodeResource(game.getContext().getResources(), R.drawable.green_button_pushed);
     }
 
     public void draw(Canvas canvas) {
-        canvas.drawCircle(centerX, centerY, radius, paint);
+        // 현재 버튼의 상태에 따라서 그리기
+        if (isPressed || !isCooldownElapsed()) {
+            canvas.drawBitmap(buttonPressedBitmap, centerX - radius, centerY - radius, null);
+        } else {
+            canvas.drawBitmap(buttonBitmap, centerX - radius, centerY - radius, null);
+        }
     }
 
     public boolean isPressed(double touchPosX, double touchPosY) {
@@ -38,8 +55,8 @@ public class SkillButton {
     }
 
     public void setIsPressed(boolean isPressed) {
-        this.isPressed = isPressed;
-        if (isPressed) {
+        if (isPressed && isCooldownElapsed()) {
+            lastClickTime = SystemClock.elapsedRealtime();
             game.onSkillButtonPressed();
         }
     }
@@ -47,4 +64,20 @@ public class SkillButton {
     public boolean getIsPressed() {
         return isPressed;
     }
+
+    private boolean isCooldownElapsed() {
+        long elapsedTime = SystemClock.elapsedRealtime() - lastClickTime;
+        return elapsedTime >= skillCooldown;
+    }
+
+    public void update() {
+        if (!isCooldownElapsed()) {
+            // 쿨타임 중일 때는 버튼을 누른 상태로 유지
+            isPressed = true;
+        } else {
+            // 쿨타임이 끝났을 때는 버튼을 누르지 않은 상태로 변경
+            isPressed = false;
+        }
+    }
 }
+
